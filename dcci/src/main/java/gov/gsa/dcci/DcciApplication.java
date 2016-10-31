bed0e37beb351e41d51c67995972a979aff8d4df
matthewreed26 <mreed@ventera.com>
2016-10-31 11:14:58 -0400

    Initial Commit for DCCi Project

---
+++ dcci/src/main/java/gov/gsa/dcci/DcciApplication.java
@@ -0,0 +1,12 @@
+package gov.gsa.dcci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcciApplication.class, args);
+	}
+}
---
+++ dcci/src/test/java/gov/gsa/dcci/DcciApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.gsa.dcci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DcciApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
