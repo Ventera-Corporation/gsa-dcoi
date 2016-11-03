6c38ff490a96039a0763201b461c8a6ca53cf9e5
jgiovanettiatventera <jgiovanetti@ventera.com>
2016-11-03 14:17:34 -0400

    Admin dashboard

---
+++ app/app.js
@@ -0,0 +1,5 @@
+var dcciApp_module = angular.module("dcciApp", []);
+
+dcciApp_module.controller("MainController", function ( $scope ) {
+        
+});
---
+++ app/common-includes/mainHeaderDirective.js
@@ -0,0 +1,6 @@
+angular.module("dcciApp").directive("mainHeaderDirective", function () {
+	
+	return {
+		templateUrl: "app/common-includes/main-header.html"
+	}
+});
