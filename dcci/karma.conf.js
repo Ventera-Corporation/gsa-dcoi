8f1ca050a4495ff5b534820cbd590f37d2901964
Ryan G Harvey <rharvey@ventera.com>
2016-12-02 12:39:16 -0500

    Update karma.conf.js

--- dcci/karma.conf.js
+++ dcci/karma.conf.js
@@ -24,7 +24,20 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/main/resources/public/plugins/**/*.js',
+      'src/main/resources/public/plugins/angularjs/angular.min.js',
+      'src/main/resources/public/plugins/angularjs/angular-route.min.js',
+      'src/main/resources/public/plugins/angularjs/angular-resource.min.js',
+      'src/main/resources/public/plugins/angularjs/angular-sanitize.min.js',
+      'src/main/resources/public/plugins/angularjs/angular-mocks.js',
+      'src/main/resources/public/plugins/angularjs/ui-bootstrap-2.0.2.min.js',
+      'src/main/resources/public/plugins/angularjs/ui-bootstrap-tpls-1.3.2.min.js',
+      'src/main/resources/public/app/module.js',
+      'src/main/resources/public/app/module-config.js',
+      'src/main/resources/public/app/dashboard/dashboard-controller.js',
+      'src/main/resources/public/app/dashboard/dashboard-service.js',
+      'src/main/resources/public/app/quarter/quarter-controller.js',
+      'src/main/resources/public/app/quarter/quarter-service.js',
+      'src/main/resources/public/app/datacenter/datacenter-controller.js',
       'src/test/**/*.js'
     ],
 
