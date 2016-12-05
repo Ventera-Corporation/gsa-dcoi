6a2382a7d42b54391ad4fe4bdedbba60bd976a4d
matthewreed26 <mreed@ventera.com>
2016-12-05 17:41:18 -0500

    #23 merged index and module-config

--- dcci/src/main/resources/public/app/module-config.js
+++ dcci/src/main/resources/public/app/module-config.js
@@ -8,35 +8,30 @@
 		$routeProvider.otherwise({
 			redirectTo : '/login'
 		}).when("/error/:code", {
-	        templateUrl: "app/common/error.html",
-	        controller: "ErrorController",
-	        controllerAs : 'ec',
-	        access : {
-			    loginRequired : true,
-			    authorizedRoles : [ USER_ROLES.all ]
+			templateUrl : "app/common/error.html",
+			controller : "ErrorController",
+			controllerAs : 'ec',
+			access : {
+				loginRequired : true,
+				authorizedRoles : [ USER_ROLES.all ]
 			}
-	    }).when('/login', {
+		}).when('/login', {
 			templateUrl : 'app/login/login.html',
 			controller : 'LoginController',
 			controllerAs : 'lc',
 			access : {
-			    loginRequired : false,
-			    authorizedRoles : [ USER_ROLES.all ]
+				loginRequired : false,
+				authorizedRoles : [ USER_ROLES.all ]
 			}
-	    }).when('/dashboard', {
+		}).when('/dashboard', {
 			templateUrl : 'app/dashboard/dashboard.html',
 			controller : 'DashboardController',
 			controllerAs : 'dc',
-			access : {
-			    loginRequired : true,
-			    authorizedRoles : [ USER_ROLES.all ]
-			},
 			resolve : {
 				dashboardData : function(QuarterService) {
-					// return QuarterService.initDashboard().then(function
-					// (data){
-					// return data.dashboardData;
-					// });
+//					return QuarterService.initDashboard().then(function (data){
+//						return data.dashboardData;
+//					});
 					return {
 						years : [ '2016', '2015', '2014' ],
 						quarters : [ {
@@ -138,171 +133,186 @@
 					return QuarterService.initQuarter().then(function(data) {
 						return data.quarterData;
 					});
+					/**return {
+						fiscalQuarterReport: {
+					    	fiscalQuarter: 'Q3',
+							fiscalYear: '2016',
+							quarterInProgressFlag: true,
+							quarterActiveFlag: false
+						},
+						regions: [
+							{
+								name: 'New England',
+								code: 'newEngland',
+								regionId: 0,
+								dataCenters: [
+									{
+										dataCenterName: 'Data Center 1 Name',
+										dataCenterId: 'Data Center 1 ID',
+										dcoiDataCenterId: 'Data Center 1 DCOI ID',
+										city: 'Data Center 1 City',
+										stateName: 'Data Center 1 State',
+										fieldOffices: [
+											{
+												name: 'PBS',
+												generalInfo: {},
+												status: {},
+												facilityInfo: {},
+												serverInfo: {}
+											},
+											{
+												name: 'FAS',
+												generalInfo: {},
+												status: {},
+												facilityInfo: {},
+												serverInfo: {}
+											}
+										],
+									},
+									{
+										dataCenterName: 'Data Center 2 Name',
+										dataCenterId: 'Data Center 2 ID',
+										dcoiDataCenterId: 'Data Center 2 DCOI ID',
+										city: 'Data Center 2 City',
+										stateName: 'Data Center 2 State',
+										fieldOffices: [
+											{
+												name: 'PBS',
+												generalInfo: {},
+												status: {},
+												facilityInfo: {},
+												serverInfo: {}
+											},
+											{
+												name: 'FAS',
+												generalInfo: {},
+												status: {},
+												facilityInfo: {},
+												serverInfo: {}
+											}
+										],
+									}
+								],
+							},
+							{
+								name: 'Northeast and Caribbean',
+								code: 'northeastAndCaribbean',
+								regionId: 1,
+								dataCenters: [
+									{
+										dataCenterName: 'Data Center 4 Name',
+										dataCenterId: 'Data Center 4 ID',
+										dcoiDataCenterId: 'Data Center 4 DCOI ID',
+										city: 'Data Center 4 City',
+										stateName: 'Data Center 4 State',
+										fieldOffices: [
+											{
+												name: 'PBS',
+												generalInfo: {},
+												status: {},
+												facilityInfo: {},
+												serverInfo: {}
+											},
+											{
+												name: 'FAS',
+												generalInfo: {},
+												status: {},
+												facilityInfo: {},
+												serverInfo: {}
+											},
+											{
+												name: 'OCIO',
+												generalInfo: {},
+												status: {},
+												facilityInfo: {},
+												serverInfo: {}
+											}
+										],
+									}
+								],
+							},
+							{
+								name: "Mid-Atlantic",
+								code: 'midAtlantic',
+								regionId: 2,
+								dataCenters: [],
+							},
+							{
+								name: "Southeast Sunbelt",
+								code: 'southeastSunbelt',
+								regionId: 3,
+								dataCenters: [],
+							},
+							{
+								name: "Great Lakes",
+								code: 'greatLakes',
+								regionId: 4,
+								dataCenters: [],
+							},
+							{
+								name: "Heartland",
+								code: 'heartland',
+								regionId: 5,
+								dataCenters: [],
+							},
+							{
+								name: "Greater Southwest",
+								code: 'greaterSouthwest',
+								regionId: 6,
+								dataCenters: [],
+							},
+							{
+								name: "Rocky Mountain",
+								code: 'rockyMountain',
+								regionId: 7,
+								dataCenters: [],
+							},
+							{
+								name: "Pacific Rim",
+								code: 'pacificRim',
+								regionId: 8,
+								dataCenters: [],
+							},
+							{
+								name: "Northwest/Arctic",
+								code: 'northwestArctic',
+								regionId: 9,
+								dataCenters: [],
+							},
+							{
+								name: "National Capital",
+								code: 'nationalCapital',
+								regionId: 10,
+								dataCenters: [],
+							},
+							{
+								name: "Cloud",
+								code: 'cloud',
+								regionId: 11,
+								dataCenters: [],
+							}
+						]
+					};**/
+				}
+			}
+		}).when('/searchResults', {
+			templateUrl : 'app/search/search.html',
+			controller : 'SearchController',
+			controllerAs : 'sc',
+			resolve : {
+				advancedSearchMode : function() {
+					return false;
+				}
+			}
+		}).when('/queryData', {
+			templateUrl : 'app/search/search.html',
+			controller : 'SearchController',
+			controllerAs : 'sc',
+			resolve : {
+				advancedSearchMode : function() {
+					return true;
 				}
 			}
 		});
-		/**return {
-			fiscalQuarterReport: {
-		    	fiscalQuarter: 'Q3',
-				fiscalYear: '2016',
-				quarterInProgressFlag: true,
-				quarterActiveFlag: false
-			},
-			regions: [
-				{
-					name: 'New England',
-					code: 'newEngland',
-					regionId: 0,
-					dataCenters: [
-						{
-							dataCenterName: 'Data Center 1 Name',
-							dataCenterId: 'Data Center 1 ID',
-							dcoiDataCenterId: 'Data Center 1 DCOI ID',
-							city: 'Data Center 1 City',
-							stateName: 'Data Center 1 State',
-							fieldOffices: [
-								{
-									name: 'PBS',
-									generalInfo: {},
-									status: {},
-									facilityInfo: {},
-									serverInfo: {}
-								},
-								{
-									name: 'FAS',
-									generalInfo: {},
-									status: {},
-									facilityInfo: {},
-									serverInfo: {}
-								}
-							],
-						},
-						{
-							dataCenterName: 'Data Center 2 Name',
-							dataCenterId: 'Data Center 2 ID',
-							dcoiDataCenterId: 'Data Center 2 DCOI ID',
-							city: 'Data Center 2 City',
-							stateName: 'Data Center 2 State',
-							fieldOffices: [
-								{
-									name: 'PBS',
-									generalInfo: {},
-									status: {},
-									facilityInfo: {},
-									serverInfo: {}
-								},
-								{
-									name: 'FAS',
-									generalInfo: {},
-									status: {},
-									facilityInfo: {},
-									serverInfo: {}
-								}
-							],
-						}
-					],
-				},
-				{
-					name: 'Northeast and Caribbean',
-					code: 'northeastAndCaribbean',
-					regionId: 1,
-					dataCenters: [
-						{
-							dataCenterName: 'Data Center 4 Name',
-							dataCenterId: 'Data Center 4 ID',
-							dcoiDataCenterId: 'Data Center 4 DCOI ID',
-							city: 'Data Center 4 City',
-							stateName: 'Data Center 4 State',
-							fieldOffices: [
-								{
-									name: 'PBS',
-									generalInfo: {},
-									status: {},
-									facilityInfo: {},
-									serverInfo: {}
-								},
-								{
-									name: 'FAS',
-									generalInfo: {},
-									status: {},
-									facilityInfo: {},
-									serverInfo: {}
-								},
-								{
-									name: 'OCIO',
-									generalInfo: {},
-									status: {},
-									facilityInfo: {},
-									serverInfo: {}
-								}
-							],
-						}
-					],
-				},
-				{
-					name: "Mid-Atlantic",
-					code: 'midAtlantic',
-					regionId: 2,
-					dataCenters: [],
-				},
-				{
-					name: "Southeast Sunbelt",
-					code: 'southeastSunbelt',
-					regionId: 3,
-					dataCenters: [],
-				},
-				{
-					name: "Great Lakes",
-					code: 'greatLakes',
-					regionId: 4,
-					dataCenters: [],
-				},
-				{
-					name: "Heartland",
-					code: 'heartland',
-					regionId: 5,
-					dataCenters: [],
-				},
-				{
-					name: "Greater Southwest",
-					code: 'greaterSouthwest',
-					regionId: 6,
-					dataCenters: [],
-				},
-				{
-					name: "Rocky Mountain",
-					code: 'rockyMountain',
-					regionId: 7,
-					dataCenters: [],
-				},
-				{
-					name: "Pacific Rim",
-					code: 'pacificRim',
-					regionId: 8,
-					dataCenters: [],
-				},
-				{
-					name: "Northwest/Arctic",
-					code: 'northwestArctic',
-					regionId: 9,
-					dataCenters: [],
-				},
-				{
-					name: "National Capital",
-					code: 'nationalCapital',
-					regionId: 10,
-					dataCenters: [],
-				},
-				{
-					name: "Cloud",
-					code: 'cloud',
-					regionId: 11,
-					dataCenters: [],
-				}
-			]
-		};
-		}
-		}
-		});**/
 	}
 })();
